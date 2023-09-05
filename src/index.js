00d056abb7e873204d9c8391737e94f2be247db2
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-09-05 21:44:05 +0700

    Welcome Page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Welcome from "./pages/Welcome/Welcome.jsx";
+import Main from "./pages/Main/Main.jsx";
+
+// report web vitals
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/main" element={<Main />} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo-sushi.png";
+
+function WelcomePage() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row align-items-center justify-content-center vh-100">
+          <div className="col-md-8 col-lg-5 col-12">
+            <div className="welcome-card bg-white border rounded">
+              <div className="px-4 py-3">
+                <div className="card-image">
+                  <img
+                    className="object-fit-contain d-block mx-auto"
+                    width="200"
+                    src={logo}
+                    alt=""
+                  />
+                </div>
+                <div className="card-text mb-3">
+                  <h3 className="title-font text-center">
+                    Welcome to Sushigiri
+                  </h3>
+                  <p className="text-center fs-5">
+                    tempat cari restoran jepang terlengkap di Semarang
+                  </p>
+                </div>
+
+                <div className="card-button mb-3">
+                  <NavLink
+                    type="button"
+                    className="btn btn-primary w-100 text-uppercase fw-bold fs-6 title-font"
+                    to="/main"
+                  >
+                    Lanjutkan
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WelcomePage;
